a0eeb908ad83c0e6ea273f406a99618c28a4fbb1
BigBook8888 <32974302+BigBook8888@users.noreply.github.com>
2018-05-25 16:56:04 +0800

    第四章第三个视频

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => { // 使用组件
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : '' // 去掉第一个&，用于拼接,例 'roomid=0123'
+}
